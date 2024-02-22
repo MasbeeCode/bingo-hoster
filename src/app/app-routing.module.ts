e91b917063d5d232f65c7af7122bb94d01e7decd
Mason Hill <masonhillsoftware@gmail.com>
2024-02-21 21:42:40 -0800

    feat: add default layout components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DefaultLayoutComponent } from './layouts/default/default-layout.component';
+import { BingDashboardComponent } from './modules/bingos/bingo-dashboard/bingo-dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    component: DefaultLayoutComponent,
+    path: '',
+    children: [
+      { path: '', redirectTo: '/bingo', pathMatch: 'full' }, //Empty route handler
+      { path: 'bingo', component: BingDashboardComponent },
+      {
+        path: '**',
+        redirectTo: '/bingo',
+        pathMatch: 'full',
+      }, //Undefined route handler
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
